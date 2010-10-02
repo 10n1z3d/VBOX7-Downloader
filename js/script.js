c62db8fd3dc57595fa0e182014ca46333f774eac
10n1z3d <10n1z3d@w.cn>
2010-10-02 21:16:26 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function createDownloadButton(data) {
+    var controlContainer = findControlContainer("clipControls");
+    
+    var controlBtn = document.createElement("div");
+    controlBtn.setAttribute("class", "clipControlsBtn");
+
+    var downloadUrl = document.createElement("a");
+    downloadUrl.setAttribute("href", data.match(/&videoFile=(.*)+&/)[1])
+    downloadUrl.setAttribute("alt", "Свали");
+
+    var downloadImage = document.createElement("img");
+    downloadImage.setAttribute("src", "chrome-extension://ipdgaigpbpgbfgdjkcieghkmchpdppod/img/download.png");
+
+    downloadUrl.appendChild(downloadImage);
+    controlBtn.appendChild(downloadUrl);
+    controlContainer.appendChild(controlBtn);
+}
+
+function findControlContainer(classname) {
+    var divs = document.getElementsByTagName("div");
+    
+    for (var i = 0; i < divs.length; i++) {
+        if (divs[i].className == classname) {
+            return divs[i];
+        }
+    }
+    
+    return null;
+}
+
+if(/http\:\/\/vbox7.com\/play\:\w+/.test(document.location.href)) {
+    chrome.extension.sendRequest({'action': 'fetchVideoUrl', 
+                                  'videoId': document.location.href.split(":")[2]}, 
+                                  createDownloadButton);
+}
